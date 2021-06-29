df4f4f62bd12d378f203073f20050343d07e3018
indranil2507 <iindranilsaha1@gmail.com>
2021-06-29 22:55:16 +0530

    first commit

---
+++ src/Screens/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "../components/Home/Hero";
+import Footer from "../components/Home/Footer";
+import "./screens.css";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <Hero />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Home/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero__text">
+        <h1>CABINS-4-U</h1>
+        <p>Find Your escape in our cabins in the lap of nature</p>
+        <div className="hero__button">BOOK YOUR CABINS</div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
