47c919d7fcd090fd3f4bbe0e6ba9d0813f14226a
jayasri dey <54766450+jayasri2000@users.noreply.github.com>
2021-08-10 13:17:57 +0530

    t

--- src/Screens/About.js
+++ src/Screens/About.js
@@ -2,6 +2,7 @@ import React from "react";
 import AboutHero from "../components/About/AboutHero";
 import Facilities from "../components/About/Facilities";
 
+
 const About = () => {
   return (
     <div style={{ backgroundColor: "#01363c", height: "100%" }}>
